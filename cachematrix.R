9741891bf305f89a214ed1568d2178c3cd7f0a17
wheatonr <rpw@SA-B013161.bom.gov.au>
2014-08-18 16:24:41 +0930

    Working version

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+## make a matrix object that caches its solution
 
-## Write a short comment describing this function
+## make a new object
+## eg m<-makeCacheMatrix(matrix(rnorm(9), nrow=3, ncol=3))
+##
+## m$get returns original matrix
+## m$getinverse returns cached inverse or NULL if not computed yet
+## m$set loads a new matrix, resets cached value
+## m$setinverse caches a new value for the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  ## init cache value to NULL
+  cached_inverse <-NULL
+  ## load new matrix, reset cache
+  set <- function(y) {
+    x <<-y
+    cached_inverse <<-NULL
+  }
+  ## return matrix
+  get <- function() x
+  ## cache inverse
+  setinverse <- function(inverse) cached_inverse <<-inverse
+  ## return inverse
+  getinverse <- function() cached_inverse
+  ## return a list of available functions
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
+  
 }
 
 
-## Write a short comment describing this function
+
+## check for a cached solution else solve
+## and return cached solution
+## eg cacheSolve(m)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## retrieve cached value
+  inverse <-x$getinverse()
+  if (!is.null(inverse)) {
+    ## inverse found in cache, nothing to do
+    message("getting inverse from cache")
+  } else {
+    # inverse not found in cache, compute and store
+    message("computing new inverse")
+    inverse <- solve(x$get())
+    x$setinverse(inverse)
+  }
+  # return inverse to caller
+  inverse
 }
